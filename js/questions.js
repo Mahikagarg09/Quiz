8df0999720f7d9d8eea01473c1be4fdf38976e76
Mahikagarg09 <mg@gwdcollective.com>
2024-04-15 19:55:35 +0530

    change ques

--- js/questions.js
+++ js/questions.js
@@ -1,57 +1,112 @@
 let questions = [
-    {
-    numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
-  },
-    {
-    numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
-  },
-    {
-    numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
-  },
-    {
-    numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
-  },
-    {
-    numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
-  },
+  {
+  numb: 1,
+  question: "What year was Maharaja Agrasen Institute of Technology (MAIT) established?",
+  answer: "B) 1999",
+  options: [
+    "A) 1997",
+    "B) 1999",
+    "C) 2001",
+    "D) 2003"
+  ]
+},
+{
+  numb: 2,
+  question: "Which university is MAIT affiliated with?",
+  answer: "A) Guru Gobind Singh Indraprastha University (GGSIPU)",
+  options: [
+    "A) Guru Gobind Singh Indraprastha University (GGSIPU)",
+    "B) Delhi Technological University (DTU)",
+    "C) Jamia Millia Islamia (JMI)",
+    "D) Jawaharlal Nehru Technological University (JNTU)"
+  ]
+},
+{
+  numb: 3,
+  question: "Which of the following is not a course offered at MAIT?",
+  answer: "D) Bioinformatics Engineering",
+  options: [
+    "A) Computer Science and Engineering",
+    "B) Electronics and Communication Engineering",
+    "C) Mechanical Engineering",
+    "D) Bioinformatics Engineering"
+  ]
+},
+{
+  numb: 4,
+  question: "Which campus is MAIT located in?",
+  answer: "B) Rohini",
+  options: [
+    "A) Dwarka",
+    "B) Rohini",
+    "C) Janakpuri",
+    "D) Saket"
+  ]
+},
+{
+  numb: 5,
+  question: "Which one of them is a cultural society of MAIT?",
+  answer: "C) Literary Umbrella",
+  options: [
+    "A) IOSD",
+    "B) GDSC",
+    "C) Literary Umbrella",
+    "D) IEEE"
+  ]
+},
+{
+  numb: 6,
+  question: "Who is the founder of Maharaja Agrasen Institute of Technology (MAIT)?",
+  answer: "C) Maharaja Agrasen Technical Education Society",
+  options: [
+    "A) Maharaja Agrasen",
+    "B) Maharaja Surajmal",
+    "C) Maharaja Agrasen Technical Education Society",
+    "D) Maharaja Agrasen Hospital"
+  ]
+},
+{
+  numb: 7,
+  question: "Which of the following sports facilities is not available at MAIT?",
+  answer: "D) Swimming Pool",
+  options: [
+    "A) Basketball Court",
+    "B) Tennis Court",
+    "C) Cricket Ground",
+    "D) Swimming Pool"
+  ]
+},
+{
+  numb: 8,
+  question: "What does NSS stand for at MAIT?",
+  answer: "A) National Service Scheme",
+  options: [
+    "A) National Service Scheme",
+    "B) National Science Society",
+    "C) National Student Society",
+    "D) National Sports Society"
+  ]
+},
+{
+  numb: 9,
+  question: "Which of the following is one of the technical societies at MAIT?",
+  answer: "A)GDSC",
+  options: [
+    "A)GDSC",
+    "B)Ayaam",
+    "C)Affinity",
+    "D)Funk in Motion"
+  ]
+},
+{
+  numb: 10,
+  question: "What is the name of the annual technical fest at MAIT?",
+  answer: "D) TnM-TechSurge n Mridang",
+  options: [
+    "A) Technotsav",
+    "B) Innovision",
+    "C) Technovision",
+    "D) TnM-TechSurge n Mridang"
+  ]
+}
 ];
